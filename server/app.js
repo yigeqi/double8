108e637cf747bc69cb9c685fff3e8d9c534a26ce
周琪 <qi.zhou@baidao.com>
2017-08-10 18:34:17 +0800

    add body-parser, less,less-loader

--- server/app.js
+++ server/app.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const path = require('path')
+var bodyParser = require('body-parser')
 
 const app = express()
+
+app.set('port', process.env.PORT || 3000);
 //server static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')))
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))
 })
 
-const PORT = process.env.PORT || 3000
-app.listen(3000, () => {
-  console.log(`app is listening on port ${PORT}.`)
+app.listen(app.get('port'), () => {
+  console.log(`app is listening on port ${app.get('port')}.`)
 })
