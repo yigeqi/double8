12917dd4ff9901a77cd01111fcc295c6f99bf41d
周琪 <qi.zhou@baidao.com>
2017-09-07 11:33:24 +0800

    增加bcrypt给用户密码加密

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -5,12 +5,16 @@ var redisClient = require('../db/redis.js')
 exports.login = (req, res) => {
   const username = req.body.username
   const password = req.body.password
-  UserModel.findOne({username,password}, (err, user) => {
+  UserModel.findOne({username}, async (err, user) => {
     if (err) {
       console.log(err)
       return res.sendStatus(500)
     }
     if (user) {
+      const isSame = await user.comparePassword(password, user.password)
+      if (!isSame) {
+        return res.json({success:false,message:'password not right'})
+      }
       const user_id = user._id.toString()
       const token = jwt.sign({id: user_id}, 'hahaha', {expiresIn: 24*60*60})
       // 有交互时要记得更新这个过期时间
@@ -39,7 +43,7 @@ exports.register = (req, res) => {
       newuser.save(err => {
         if (err) {
           console.log(err)
-          return res.sendStatus(500)
+          return res.status(500).send({success:false,message:err.message})
         }
         return res.sendStatus(200)
       })
--- server/db/userModel.js
+++ server/db/userModel.js
@@ -1,11 +1,29 @@
 const mongoose = require('mongoose')
-var timestamps    = require('mongoose-timestamp')
+const timestamps  = require('mongoose-timestamp')
+const bcrypt = require('bcrypt')
 
+const saltRounds = 8
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 })
 UserSchema.plugin(timestamps)
+// 把用户的password编码后再存入数据库
+UserSchema.pre('save', function(next) {
+  const user = this
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    if (err) return next(err)
+    bcrypt.hash(user.password, salt, (err, hash)=>{
+      if (err) return next(err)
+      user.password = hash
+      next()
+    })
+  })
+})
+// 比较明文密码是否和数据库中密码相同，返回true/false
+UserSchema.methods.comparePassword = (beforeBcryptPsd,bcryptPsd) => {
+  return bcrypt.compare(beforeBcryptPsd, bcryptPsd).then(res=>res)
+}
 
 const UserModel = mongoose.model('User', UserSchema)
 module.exports = UserModel
