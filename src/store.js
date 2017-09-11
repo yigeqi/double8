8d1d0a3a7f6667770a6abf696bb4b0eb3925f14e
周琪 <qi.zhou@baidao.com>
2017-09-11 15:15:21 +0800

    增加redux-logger’

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 import thunkMiddleware from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
+import logger from 'redux-logger'
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunkMiddleware)
+  applyMiddleware(thunkMiddleware,logger)
 )
 
 export default store
