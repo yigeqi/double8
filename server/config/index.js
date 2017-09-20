fec5f43ceff94e0731ae93e22f321f75909bb718
yigeqi <firefly126@126.com>
2017-09-20 16:02:12 +0800

    修改webpack-dev-server的port由3000为3456

--- server/config/index.js
+++ server/config/index.js
@@ -3,7 +3,7 @@ module.exports={
   mongodb: 'mongodb://127.0.0.1:27017/double8',
   redisPort: 6379,
   redisDb: '127.0.0.1',
-  allowSite: ['http://127.0.0.1:3000','http://127.0.0.1:8080'],
+  allowSite: ['http://127.0.0.1:3456','http://127.0.0.1:8080'],
   maxBytes: 5000,
   secret: 'hahaha',
   expire: 8*60*60 // 秒为单位
